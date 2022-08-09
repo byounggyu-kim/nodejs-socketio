f9f9e0a66495b3c4b85e41ba57f3616aebccc6d6
byounggyu-kim <briankim0916@gmail.com>
2022-08-09 21:06:02 +0900

    Add: socketio making rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,42 +1,17 @@
-const messageList = document.querySelector("ul");
-const sendNickForm = document.querySelector("#nick");
-const sendMessageForm = document.querySelector("#message");
+const socket = io();
 
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.querySelector("#welcome");
+const form = welcome.querySelector("form");
 
-const makeJSON = (type, payload) => {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-};
-
-socket.addEventListener("open", () => {
-  console.log("Successfully connected to the server**");
-});
-
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-// 서버가 작동되다가 꺼지면 작동이 된다. 연결이 끊어지기 때문에
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server");
-});
-
-const handleSubmit = (e) => {
+const handleRoomSubmit = (e) => {
   e.preventDefault();
-  const input = sendMessageForm.querySelector("input");
-  socket.send(makeJSON("message", input.value));
+  const input = form.querySelector("input");
+  socket.emit("room", { payload: input.value }, callback);
   input.value = "";
 };
 
-const handleNick = (e) => {
-  e.preventDefault();
-  const input = sendNickForm.querySelector("input");
-  socket.send(makeJSON("nickname", input.value));
-  input.value = "";
+const callback = () => {
+  console.log("done");
 };
 
-sendNickForm.addEventListener("submit", handleNick);
-sendMessageForm.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -12,35 +12,44 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
+const io = SocketIO(server);
 
-const onServerClose = () => {
-  console.log("hey sth got wrong from Browser");
-};
-
-const showMessage = (mes, soc) => {
-  const message = JSON.parse(mes);
-  switch (message.type) {
-    case "message":
-      sockets.forEach((el) => el.send(`${soc.nickname}: ${message.payload}`));
-      break;
-    case "nickname":
-      soc["nickname"] = message.payload;
-      break;
-  }
-};
-
-const sockets = [];
-
-const handleConnection = (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "??";
-  console.log("Successfully connected to Browser"); // 프런트와 연결 되어있을때의 로직
-  socket.send("hello!"); // 서버에서 프런트로 보내는 메세지
-  socket.on("close", onServerClose); // 프런트와의 연결이 끊긴 후의 로직
-  socket.on("message", (msg) => showMessage(msg, socket)); // 프런트에서 보낸 메세지 받는 로직
-};
-
-const wss = new WebSocket.Server({ server });
-wss.on("connection", handleConnection);
+io.on("connection", (socket) => {
+  socket.on("room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 5000);
+  });
+});
 
 server.listen(3000, handleListen);
+
+// const onServerClose = () => {
+//   console.log("hey sth got wrong from Browser");
+// };
+
+// const showMessage = (mes, soc) => {
+//   const message = JSON.parse(mes);
+//   switch (message.type) {
+//     case "message":
+//       sockets.forEach((el) => el.send(`${soc.nickname}: ${message.payload}`));
+//       break;
+//     case "nickname":
+//       soc["nickname"] = message.payload;
+//       break;
+//   }
+// };
+
+// const sockets = [];
+
+// const handleConnection = (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "??";
+//   console.log("Successfully connected to Browser"); // 프런트와 연결 되어있을때의 로직
+//   socket.send("hello!"); // 서버에서 프런트로 보내는 메세지
+//   socket.on("close", onServerClose); // 프런트와의 연결이 끊긴 후의 로직
+//   socket.on("message", (msg) => showMessage(msg, socket)); // 프런트에서 보낸 메세지 받는 로직
+// };
+
+// wss.on("connection", handleConnection);
